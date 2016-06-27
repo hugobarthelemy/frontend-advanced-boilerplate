745a6540652774955d4b56e2eaa36241ca8965c1
hugo barthelemy <barthelemyhugo@gmail.com>
2016-06-27 15:50:09 +0200

    génération page HTML ok + db completed

--- config.rb
+++ config.rb
@@ -31,6 +31,14 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+# data.flats.each do |name|
+#   proxy "#{name}.html", "show.html", :locals => { :owner_name => name }, :ignore => true
+# end
+#
+data.flats.each do |name, info|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner_name => name }, :ignore => true
+end
+
 ###
 # Helpers
 ###
